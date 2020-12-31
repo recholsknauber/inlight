4aeac6553c8d8de60b9458990dcb7d59b17fa329
Ryan Knauber <ryan.knauber@lifeworksnw.org>
2020-12-31 15:53:29 -0800

    Updating dep versions.

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
 (defproject inlight "0.1.0"
   :description "A Clojure Data Science Environment"
   :url "https://github.com/recholsknauber/inlight"
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [kixi/stats "0.5.4"]
                  [incanter "1.9.3"]
-                 [metasoarous/oz "1.6.0-alpha31"]
-                 [clj-http "3.10.3"]
+                 [metasoarous/oz "1.6.0-alpha34"]
+                 [clj-http "3.11.0"]
                  [org.clojure/data.json "1.0.0"]
                  ;;Database Stuff
+                 [seancorfield/next.jdbc "1.1.613"]
+                 [sqlingvo "0.9.34"]
                  [com.h2database/h2 "1.4.200"]
-                 [seancorfield/next.jdbc "1.1.610"]
                  [org.xerial/sqlite-jdbc "3.7.2"]
-                 [sqlingvo "0.9.31"]
                  ])
