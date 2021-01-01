971c8c2787d06c5dcf12389dc40a1c7e533f5249
Ryan Knauber <ryan.knauber@lifeworksnw.org>
2021-01-01 10:26:27 -0800

    Ignorance.

--- resources/public/js/cljs-runtime/inlight.frontend.app.js
+++
@@ -1,6 +0,0 @@
-goog.provide('inlight.frontend.app');
-inlight.frontend.app.init = (function inlight$frontend$app$init(){
-return cljs.core.println.cljs$core$IFn$_invoke$arity$variadic(cljs.core.prim_seq.cljs$core$IFn$_invoke$arity$2(["Howdy World!"], 0));
-});
-
-//# sourceMappingURL=inlight.frontend.app.js.map
--- resources/public/js/cljs-runtime/shadow.module.main.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('main');
-
-try { inlight.frontend.app.init(); } catch (e) { console.error("An error occurred when calling (inlight.frontend.app/init)"); throw(e); }
